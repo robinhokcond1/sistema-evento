c76f910b657444e876da4df51010f4a949d2b616
Robinhokcond1 <robinhokcond1@hotmail.com>
2025-04-01 21:52:56 -0300

    🚀 Projeto Spring Boot inicial - sistema-evento

---
+++ src/main/java/com/devsuperior/sistemaevento/SistemaEventoApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistemaevento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEventoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/sistemaevento/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sistemaevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
